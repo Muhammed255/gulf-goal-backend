4bcdfaedb172b8f3c3062b779fc8c26f2fc46dbb
Mohamed Ahmed <programmer2011515616@gmail.com>
2020-11-19 16:23:16 +0200

    ddd

--- controllers/teams.controller.js
+++ controllers/teams.controller.js
@@ -93,7 +93,7 @@ export default {
       });
 
       if(result.length < 1) {
-        res.status(400).json({success: false, msg: "لا يوجد مبارايات الان !!!"});
+        res.status(200).json({success: false, msg: "لا يوجد مبارايات الان !!!"});
       }
 
       res.status(200).json({
