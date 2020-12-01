2377bf562b0d4db386a203f5a269dbbec1bf9f29
Mohamed Ahmed <programmer2011515616@gmail.com>
2020-12-01 23:48:34 +0200

    api key update

--- middleware/app-config.js
+++ middleware/app-config.js
@@ -1,6 +1,6 @@
 export const appConfig = {
   football_API_KEY:
-    "277eaabfa4cd25e30e3e7b3eb96756c2e470d6a20c0e73835663ddc69c50afbb",
+    "290ec875c73e45bba490754b61ba1c1dabf300d2d30d5dd81bb25eab35f59a16",
   securityCode: "$#GR24T4344$#$@#%WTEWTEAE%$6",
   database:
     "mongodb+srv://Mhmd:cs6kwkD7aduEmSZ@cluster0.8xdx0.mongodb.net/gulf-goal?retryWrites=true&w=majority",
