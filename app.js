d696b83cbf4ae5e8d7935ed02b3e08fb1bc7f23d
Mohamed Ahmed <programmer2011515616@gmail.com>
2021-01-18 17:42:47 +0200

    Redeploying

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 //Setup CORS
 
 app.use(cors());
-
+/*
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
@@ -86,7 +86,7 @@ app.use((req, res, next) => {
     "GET, POST, PATCH, PUT, DELETE, OPTIONS"
   );
   next();
-});
+});*/
 
 app.use("/api/users", userRoutes);
 
