761a9f9ab1150bdfa459acf4af95177a6cc7d770
Mohamed Ahmed <programmer2011515616@gmail.com>
2021-01-18 02:48:19 +0200

    ddd

--- app.js
+++ app.js
@@ -68,21 +68,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // });
 
 //Setup CORS
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
 
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET,PUT,POST,DELETE,OPTIONS, PATCH"
-  );
-
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-
-  next();
-});
+app.use(cors);
 
 app.use("/api/users", userRoutes);
 
