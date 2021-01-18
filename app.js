51579fb89b63079914b187672fe3b093fa8cb84a
Mohamed Ahmed <programmer2011515616@gmail.com>
2021-01-18 16:52:28 +0200

    Redeploying

--- app.js
+++ app.js
@@ -71,6 +71,21 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(cors());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  next();
+});
+
 app.use("/api/users", userRoutes);
 
 app.use("/api/news", newsRoutes);
