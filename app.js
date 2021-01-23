6dca4f2aeb55e4c4cdc0505254650f0ba77302ce
Muhammed255 <programmer2011515616@gmail.com>
2021-01-24 00:35:07 +0200

    sss

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    "Access-Control-Allow-Origin, Access-Control-Allow-Headers, X-Requested-With, Content-Type, Accept, Authorization"
   );
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Max-Age", "1800");
