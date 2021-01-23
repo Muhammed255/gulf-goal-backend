e785f23476c9ba7f31525324bcaa4b6f5ef64a2c
Muhammed255 <programmer2011515616@gmail.com>
2021-01-23 17:44:20 +0200

    Error happens in cors

--- controllers/news.controller.js
+++ controllers/news.controller.js
@@ -258,7 +258,7 @@ export default {
       res.status(200).json({ success: true, msg: "News Updated !!" });
     } catch (err) {
       console.log(err);
-      res.status(500).json({ success: false, msg: "Error Occured !!", err });
+      res.status(500).json({ success: false, msg: "Error Occured: " + err });
     }
   },
 
